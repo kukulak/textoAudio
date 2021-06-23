99078a4c61b8cc77e5170a0732eaaacdaa6995f8
Christian Valderrama <valderrama@valderrama.local>
2021-06-23 18:19:05 -0500

    primer commit

---
+++ README.md
@@ -0,0 +1,44 @@
+<img src="https://raw.githubusercontent.com/kukulak/frutas/main/img/pantallazoTextoAudio.jpg" alt="TextoAudio" width="838" height="512" >
+
+
+## de Texto a Audio
+**Creado con javaScript Vainilla**
+
+## Objetivo
+
+Explorar Audio()
+
+
+## Idea
+
+ ¿Cuantas palabras tiene el léxico de una persona común?
+ Buscando en internet se encuentran resultados variados; 300, 500, 1000 ó miles.
+ Pero el problema con el que me encontré no es la palabra como unidad, el problema son sus derivados, conjugaciones, géneros, etcétera.
+ Esto lo transforma en un proyecto interminable.
+
+#### Mutación del proyecto.
+
+¿Por qué no invitar a la gente que grabe las palabras que faltan?
+al escuchar el audio con diferentes voces lo lleva a algo más interesante,
+"un texto leido por muchas personas" al fin de cuantas son las palabras que todas las personas de habla hispana usan.
+
+
+## Tareas
+
+- [x] Implementar lógica básica de lectura de archivos de audio
+- [ ] Quitar Bootstrap
+- [ ] Mejorar UX/UI
+- [ ] Usar RegEx para lectura correcta de archivos
+- [ ] Subir Mínimo Producto Viable
+- [ ] Implementar grabación de audio desde la página
+- [ ] Agregar animación que represente el audio
+
+## Contacto
+
+Si tienes alguna observación o comentario <kukulak.cero@gmail.com>
+
+
+## Licencia
+
+Proyecto con Licencia [MIT](http://www.opensource.org/licenses/mit-license.php) 
+
---
+++ textoAudio.js
@@ -0,0 +1,145 @@
+
+
+var x
+
+var audio
+
+var parrafo
+
+var nextWord = 1;
+
+var fileDoesNotExist = function(whosError, afterError, index){
+	        whosError.onerror = function(){
+	        	document.getElementById("noHay").innerHTML += afterError[index] + " ";
+	        	console.log(afterError[index])
+	        	index++;
+	        	audio.src="audiomp3" + "/" + "nohay" + "-01" + ".mp3";
+	        	audio.play();
+
+	        };
+	        }
+
+
+
+
+var contador = 1
+
+function crearNodoHijo(nodoPadre, mensaje) {
+	var nodoHijo = document.createElement("div");
+	nodoHijo.className = "nuevoMensaje"
+	nodoHijo.onclick = function(){unaFuncion(event);};
+//	
+	var h = document.createElement("P");
+//
+	var nodoTexto = document.createTextNode(mensaje);
+//	
+	h.appendChild(nodoTexto);
+//
+	nodoHijo.appendChild(h);
+
+	nodoPadre.appendChild(nodoHijo);
+
+	var abajo = document.getElementById("aquiLosTextos");
+
+	contador = contador +1;
+
+	function scrollMe(){
+		var divTall = document.getElementById("aquiLosTextos").scrollHeight;
+
+		document.getElementById("aquiLosTextos").scrollTop += divTall;
+
+
+	}
+
+	scrollMe()
+
+}
+
+
+
+function myFunction() {
+		x = document.getElementById("aquiElTexto").value;
+
+		parrafo = x.split(" ");
+
+		var index = 1;
+
+
+	audio = new Audio("audiomp3" + "/" + parrafo[0] + "-01" + ".mp3");
+	audio.play();
+	fileDoesNotExist(audio, parrafo, 0);
+
+	audio.onended = function() {
+		    if(index < parrafo.length){
+		        audio.src="audiomp3" + "/" + parrafo[index] + "-01" + ".mp3";
+		        fileDoesNotExist(audio, parrafo, index);
+		        index++;
+		        audio.play();	        
+		    } 
+		};
+
+		document.getElementById("aquiElTexto").value = "";
+
+		crearNodoHijo(document.getElementById("aquiLosTextos"), x);
+
+
+
+}
+
+
+function unaFuncion(event) { 
+
+    var reListen = event.target.innerHTML
+    listenFunction(reListen);
+}
+
+function listenFunction(objetoEscuchar) {
+		x = objetoEscuchar;
+
+		parrafo = x.split(" ");
+
+		var index = 1;
+
+
+	audio = new Audio("audiomp3" + "/" + parrafo[0] + "-01" + ".mp3");
+	audio.play();
+	fileDoesNotExist(audio, parrafo, 0);
+
+	audio.onended = function() {
+		    if(index < parrafo.length){
+		        audio.src="audiomp3" + "/" + parrafo[index] + "-01" + ".mp3";
+		        fileDoesNotExist(audio, parrafo, index);
+		        index++;
+		        audio.play();	        
+		    } 
+		};
+
+
+	console.log("escribi algo si me ven si funciono");
+
+
+
+}
+
+
+document.getElementById("aquiElTexto").onkeypress = function(e){
+    if (!e) e = window.event;
+    if (e.keyCode == '13'){
+   
+      myFunction();
+      return false;
+
+    }
+  };
+
+
+
+
+document.getElementById("escuchar").onclick = function() {
+	myFunction()
+};
+
+
+
+
+
